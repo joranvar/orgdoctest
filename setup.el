bcf41beee208f2a6d4ffe0f25736a21c59ed3b04
joranvar <bart.post@gmail.com>
2017-10-17 13:05:14 +0200

    Add packages to Emacs setup

---
+++ setup.el
@@ -0,0 +1,20 @@
+;;; setup --- Setup for compiling org to html
+
+;;; Commentary:
+;;; Only used for setting up Emacs in order to be able to call (org-html-export-as-html).
+
+;;; Code:
+
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package htmlize)
+;;; setup.el ends here
